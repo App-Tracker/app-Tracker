85e5175a81f7111da64100a4ab4815863f6b1017
mariodandrea <mariodandrea14@yahoo.com>
2020-05-04 15:25:06 -0400

    Installed correct dependencies

---
+++ src/server/models/pg.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+
+const myURI =
+  'postgres://gcuggcel:qaXg6iKVoFuNmdzTvya64ldEtUepD799@drona.db.elephantsql.com:5432/gcuggcel';
+
+const URI = process.env.PG_URI || myURI;
+
+const pool = new Pool({
+  connectionString: URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
