9230c752e77c8da05e65e9c5e741127bb4dd6c9b
Sara Chang <sarachang530@gmail.com>
2020-05-04 23:27:54 -0400

    created clientid for oauth

---
+++ src/server/passportOauth/passport-setup.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: "959437448424-at7lde35gs414lq0s7ru5h92l7jrap76.apps.googleusercontent.com",
+    clientSecret: "pUGWncI1Trmf3JtTX8_wZr8n",
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
+
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
