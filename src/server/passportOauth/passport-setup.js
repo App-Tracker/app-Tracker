249c902700151236a8d2be6bcd12554a26b5d688
Sara Chang <sarachang530@gmail.com>
2020-05-05 00:41:45 -0400

    completed all oauth except database input

--- src/server/passportOauth/passport-setup.js
+++ src/server/passportOauth/passport-setup.js
@@ -1,6 +1,26 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+passport.serializeUser((user, done) => {
+  // done(null, user.id);  // uncomment when db is created
+  done(null, user) // delete
+});
+
+
+// passport.session will take cookie and pass in deserializeUSer func, take id from cookie
+// then find user in db, then select entire user object( entire user obj will be in req.user)
+// passport.deserializeUser((id, done) {    // uncomment when db is created
+//   User.findById(id, (err, user) => {
+//     done(err, user);
+//   });
+// });
+
+// delete when db is created
+passport.deserializeUser((user, done) => {
+  done(null, user);
+})
+
+
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
@@ -8,13 +28,15 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: "959437448424-at7lde35gs414lq0s7ru5h92l7jrap76.apps.googleusercontent.com",
     clientSecret: "pUGWncI1Trmf3JtTX8_wZr8n",
-    callbackURL: "http://www.example.com/auth/google/callback"
+    callbackURL: "http://localhost:3000/google/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
-  }
-));
+  (accessToken, refreshToken, profile, done) => {
+    // use profile info (mainly profile id) to check if user is registered in db 
+      //  User.findOrCreate({ googleId: profile.id }, (err, user)=> {
+      //    return done(err, user);
+      return done(null, profile) //delete & uncomment when db is created
+       }));
 
+
+module.exports = passport;
 
